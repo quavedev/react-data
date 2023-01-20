21cf7fad9cca24a8ce562539f7956df3675a302b
filipenevola <filipe@quave.com.br>
2023-01-20 06:23:22 -0400

    Upgrades to `quave:alert-react-tailwind` 3.0.0 which upgrades `@heroicons/react` to 2. As all the icons locations were changed this is a breaking change.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 3.0.0 (2023-01-20)
+
+- Upgrades to `quave:alert-react-tailwind` 3.0.0 which upgrades `@heroicons/react` to 2. As all the icons locations were changed this is a breaking change.
+
+If you are not using `@heroicons/react` 2 don't upgrade.
+
 ## 2.0.1 (2022-06-17)
 
 - Check for the existence of `result` (the return of `useFind`) before accessing `result.fetch`.
--- package.js
+++ package.js
@@ -3,17 +3,17 @@
 Package.describe({
   name: 'quave:react-data',
   summary: 'Utilities to manage data with React',
-  version: '2.0.1',
+  version: '3.0.0',
   git: 'https://github.com/quavedev/react-data',
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('2.5.6');
+  api.versionsFrom('2.10.0');
 
   api.use('ecmascript');
 
-  api.use('react-meteor-data@2.4.0');
-  api.use('quave:alert-react-tailwind@2.0.0');
+  api.use('react-meteor-data@2.6.1');
+  api.use('quave:alert-react-tailwind@3.0.0');
 
   api.mainModule('react-data.js', 'client');
   api.mainModule('server.js', 'server');
